0fd655203548162cd85256ec06c4443023defb83
Luux2 <293428@via.dk>
2025-02-07 18:22:06 +0100

    Deploy

--- src/utils/axiosBase.tsx
+++ src/utils/axiosBase.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-    baseURL: 'coachhub-api-chcag8d0bcdqatf4.westeurope-01.azurewebsites.net/api/',
+    baseURL: 'coachhub-api-chcag8d0bcdqatf4.westeurope-01.azurewebsites.net/api',
     timeout: 5000,
 });
 
