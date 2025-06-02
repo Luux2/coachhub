9097f2961b8aeeb19b5485015e4cf25aa93cd844
Luux2 <293428@via.dk>
2025-06-02 21:31:56 +0200

    småændringer

--- src/utils/axiosBase.tsx
+++ src/utils/axiosBase.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-    baseURL: 'http://localhost:3001/api',  //'https://coachhub-api.onrender.com/api',
+    baseURL: /*'http://localhost:3001/api',*/  'https://coachhub-api.onrender.com/api',
     timeout: 5000,
 });
 
