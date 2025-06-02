f78b5578c7e234475bd81a953da197ddd43a6918
Luux2 <293428@via.dk>
2025-06-02 16:59:07 +0200

    test

--- server/server.js
+++ server/server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const cors = require('cors');
 const dataRoutes = require('./routes/dataRoutes.js');
 
-
 const app = express();
 const PORT = process.env.PORT || 3001;
 
